b4dffb02bda1066aa3605a07ae27b97f985c6b5c
kratoshakan <hakansozan.inci@gmail.com>
2023-02-13 14:37:41 +0300

    asd

--- Src/EXPANDER/MAINEXPANDER/Expander.c
+++
@@ -1,9 +0,0 @@
-#include "../../../include/minishell.h"
-
-void	expander(void)
-{
-	if (g_core.exec_output == 2)
-		return ;
-	expand_values_from_lexlist();
-	clear_void_contents();
-}
--- Src/PARSER/MAINPARSER/Parser.c
+++
@@ -1,9 +0,0 @@
-#include "../../../include/minishell.h"
-
-void	parser(void)
-{
-	g_core.cmd_table = NULL;
-	create_cmdtable(g_core.lex_table);
-	fill_cmdtable();
-	create_files(g_core.cmd_table);
-}
