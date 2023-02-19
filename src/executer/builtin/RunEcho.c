e4cf50497ec9f617118a4e97a6aca47b00df025e
kratoshakan <hakansozan.inci@gmail.com>
2023-02-19 13:58:16 +0300

    jghj

--- src/executer/builtin/RunEcho.c
+++ src/executer/builtin/RunEcho.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RunEcho.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsozan <hsozan@42kocaeli.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 13:31:26 by hsozan            #+#    #+#             */
+/*   Updated: 2023/02/19 13:31:26 by hsozan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../include/minishell.h"
 
 void	run_echo(t_cmdlist *cmd_node)
@@ -17,11 +29,10 @@ void	run_echo(t_cmdlist *cmd_node)
 	while (path[i])
 	{
 		if (path[i] && str_compare(path[i], "-n") != 1)
-			write(cmd_node->outfile, path[i], ft_strlen(*path));
+			write(cmd_node->outfile, path[i], ft_strlen(path[i]));
 		if (path[i++])
-			if(str_compare(path[i-1], "-n") != 1)
+			if (path[i] && str_compare(path[i-1], "-n") != 1)
 				write(cmd_node->outfile, " ", 1);
-		
 	}
 	if (!is_n)
 		write(cmd_node->outfile, "\n", 1);
