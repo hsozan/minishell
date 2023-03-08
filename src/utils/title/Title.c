48f1a110d6dea8fe6e3993f996884019038a0b82
kratoshakan <hakansozan.inci@gmail.com>
2023-03-08 12:30:21 +0300

    title leaks

--- src/utils/title/Title.c
+++ src/utils/title/Title.c
@@ -16,14 +16,24 @@ void	change_title(void)
 {
 	char	cwd[256];
 	char	*tmp;
+	char	*tmp2;
 
-	tmp = getcwd(cwd, 256);
 	if (g_core.title.full_title)
 		free(g_core.title.full_title);
 	g_core.title.full_title = NULL;
-	own_strjoin(&g_core.title.full_title, g_core.title.head);
-	own_strjoin(&g_core.title.full_title, strrchr(tmp, '/'));
-	own_strjoin(&g_core.title.full_title, "$ ");
+	tmp = ft_strtonl(g_core.title.head);
+	tmp2 = getcwd(cwd, 256);
+	own_strjoin1(&tmp, "-\033[0;32m[\033[0m");
+	ft_printf("\nfasfasfasf\n");
+	own_strjoin1(&tmp, strrchr(tmp2, '/')+1);
+	own_strjoin1(&tmp, "\033[0;32m]\n");
+	own_strjoin1(&tmp,"\033[0;36m└──");
+	own_strjoin1(&tmp,"\033[0;32m╼\033[0;36m$");
+	own_strjoin(&g_core.title.full_title, tmp);
+	own_strjoin(&g_core.title.full_title, "\033[0m");
+	if (tmp)
+		free(tmp);
+
 }
 
 /*
@@ -38,12 +48,35 @@ void	change_title(void)
 ///	own_strjoin(&g_core.title.full_title, g_core.title.head);
 //	own_strjoin(&g_core.title.full_title, (char *)getcwd(cwd, 256));
 //	own_strjoin(&g_core.title.full_title, "$ ");
-void	set_title(void)
+/*void	set_title(void)
 {
 	g_core.title.head = NULL;
 	g_core.title.full_title = NULL;
 	own_strjoin(&g_core.title.head, "\033[0;36m▌│█║▌║▌║\033[0;32mᗰ ");
 	own_strjoin(&g_core.title.head, TITLE);
 	own_strjoin(&g_core.title.head, "\033[0;32m");
+	own_strjoin(&g_core.title.full_title, g_core.title.head);
+}*/
+void	set_title(void)
+{
+	t_env	*temp_env;
+	char *usr;
+
+	temp_env = g_core.env_table;
+	while(temp_env)
+	{
+		if(str_compare("USER", temp_env->env_name))
+			usr=temp_env->content;
+		temp_env=temp_env->next;
+	}
+	g_core.title.head = NULL;
+	g_core.title.full_title = NULL;
+	own_strjoin(&g_core.title.head, "\033[0;36m┌──\033[0;32m(\033[0;34m");
+	own_strjoin(&g_core.title.head, usr);
+	own_strjoin(&g_core.title.head,"\033[0;32m@\033[0;34m" );
+	own_strjoin(&g_core.title.head ,"minishell\033[0;32m)\n");
+	own_strjoin(&g_core.title.head,"\033[0;36m└──");
+	own_strjoin(&g_core.title.head,"\033[0;32m╼\033[0;36m$");
+	own_strjoin(&g_core.title.head, "\033[0m");
 	own_strjoin(&g_core.title.full_title, g_core.title.head);
 }
