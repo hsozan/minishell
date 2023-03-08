f0d991ef52067fc3c3d900a56597cb5fcf4f212c
kratoshakan <hakansozan.inci@gmail.com>
2023-03-08 16:30:11 +0300

    macrofix

--- src/lexer/classify/Classify.c
+++ src/lexer/classify/Classify.c
@@ -29,7 +29,7 @@ void	classify(t_lexlist *lex_table)
 	{
 		metachars = compare_metachars(lex_table->content);
 		if (metachars)
-			lex_table->type = (*metachars - 1) * ft_strlen(metachars);
+			lex_table->type = (*metachars) * ft_strlen(metachars);
 		else
 			lex_table->type = 0;
 		lex_table = lex_table->next;
