16963b1ff21a1502e66f54cc6bedbc3e31374718
kratoshakan <hakansozan.inci@gmail.com>
2023-03-09 13:25:32 +0300

    titleusrname

--- src/main/init.c
+++ src/main/init.c
@@ -12,10 +12,24 @@
 
 #include "../../include/minishell.h"
 
+void get_usrname()
+{
+	t_env	*temp_env;
+
+	temp_env = g_core.env_table;
+	while(temp_env)
+	{
+		if (str_compare("USER", temp_env->env_name))
+			g_core.usrname=temp_env->content;
+		temp_env=temp_env->next;
+	}
+}
+
 void	init_core(char **env)
 {
 	fill_envs(env);
 	set_metachars();
+	get_usrname();
 	set_title();
 	g_core.exec_output = 0;
 	g_core.old_exec_output = 0;
